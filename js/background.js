3ba86e2491255e40bbf1f636f992ccc774533927
MatissesProjects <devintfinkel@gmail.com>
2019-12-11 12:10:13 -0800

    Setting up folder structure

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+    console.log("New background available!");
   });
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
@@ -11,5 +11,9 @@ chrome.runtime.onInstalled.addListener(function() {
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
+  });
+
+  chrome.commands.onCommand.addListener(function(command) {
+    console.log('Command:', command);
   });
 });
